3e9227a85fc274c44a666848a771ef9e4e22e202
fernando <fernandomoran323@gmail.com>
2025-02-03 08:38:07 -0800

    refactor: improved the statics files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  base_url_api: "http://127.0.0.1:3000/api",
-  backend_host: "http://127.0.0.1:3000"
+  base_url_api: "https://hero-app-api-production.up.railway.app/api",
+  backend_host: "https://hero-app-api-production.up.railway.app
 };
+
+// https://hero-app-api-production.up.railway.app/
